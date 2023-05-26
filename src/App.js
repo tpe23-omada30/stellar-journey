9800876b1065d5a4db0a66a70a374f55c9126b73
flafflar <achmichail@gmail.com>
2023-05-26 18:57:35 +0300

    fix: update the question when the URL changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
+
 import Questions from './data/Questions';
 import PlanetQuestion from './containers/PlanetQuestion';
 
 import './App.css';
 
 function App() {
+
 	// Get the planet from the hash of the URL
-	let planet = window.location.hash.slice(1);
+	let [planet, setPlanet] = useState(window.location.hash.slice(1));
+
+	// Automatically update the planet when the URL changes
+	useEffect(() => {
+		function updatePlanet(){
+			setPlanet(window.location.hash.slice(1));
+		}
+		window.addEventListener("hashchange", updatePlanet);
+		return () => window.removeEventListener("hashchange", updatePlanet);
+	})
 
 	// Check if the planet is valid
 	if (planet in Questions){
