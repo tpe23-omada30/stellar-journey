f583a1e89e6043bbafd883f886ac83f4ab991ad6
flafflar <achmichail@gmail.com>
2023-05-26 18:45:57 +0300

    feat: get the planet to ask questions for from the hash of the URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import PlanetQuestion from './containers/PlanetQuestion';
+
 import './App.css';
 
 function App() {
-	return null;
+	// Get the planet from the hash of the URL
+	let planet = window.location.hash.slice(1);
+
+	return <PlanetQuestion planet={planet}></PlanetQuestion>;
 }
 
 export default App;
