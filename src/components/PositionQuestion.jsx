f36ef45a5487372bae9c2e27b8a54d2b743aca7f
flafflar <achmichail@gmail.com>
2023-05-26 17:51:09 +0300

    feat: add question for the position of a planet

---
+++ src/components/PositionQuestion.jsx
@@ -0,0 +1,26 @@
+import "./MultipleChoiceQuestion.css"
+import "./PositionQuestion.css"
+
+const PLANETS = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+const SIZES =   [0.09,      0.27,    0.3,    0.15,   0.9,        0.7,      0.32,     0.31];
+
+export default function PositionQuestion() {
+	return <div className="position-question multiple-choice-question">
+		<div className="question">Σε ποια θέση βρίσκεται ο [πλανήτης];</div>
+		<div className="planets">{
+			PLANETS.map((planet, i) => (<>
+				<div className="square">
+					<div className="square-content center">
+						<div className="planet" style={{
+							width: `${SIZES[i]*100}%`,
+							height: `${SIZES[i]*100}%`
+						}}></div>
+					</div>
+				</div>
+				<div className="numeral">{i+1}</div>
+			</>))
+		}{
+			PLANETS.map((planet, i) => null)
+		}</div>
+	</div>
+}
