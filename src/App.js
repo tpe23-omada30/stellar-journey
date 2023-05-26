e8544f865a6c4faa9b9221ff78dd3f07cf416b8f
flafflar <achmichail@gmail.com>
2023-05-26 18:51:48 +0300

    fix(app): check if the planet in the hash is valid

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Questions from './data/Questions';
 import PlanetQuestion from './containers/PlanetQuestion';
 
 import './App.css';
@@ -6,7 +7,13 @@ function App() {
 	// Get the planet from the hash of the URL
 	let planet = window.location.hash.slice(1);
 
-	return <PlanetQuestion planet={planet}></PlanetQuestion>;
+	// Check if the planet is valid
+	if (planet in Questions){
+		return <PlanetQuestion planet={planet}></PlanetQuestion>;
+	} else {
+		return null;
+	}
+
 }
 
 export default App;
